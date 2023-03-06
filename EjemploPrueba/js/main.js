650416633227941256409e2ba38ef1dd04ffbec6
oyarzun8 <127136072+oyarzun8@users.noreply.github.com>
2023-03-06 20:17:03 +0100

    Update main.js

--- EjemploPrueba/js/main.js
+++ EjemploPrueba/js/main.js
@@ -286,6 +286,103 @@ function cleanIntersected() {
 
 //
 
+function initSkinnedMesh() {
+
+    const segmentHeight = 5;
+    const segmentCount = 5;
+    const height = segmentHeight * segmentCount;
+    const halfHeight = height * 0.5;
+
+    const sizing = {
+            segmentHeight,
+            segmentCount,
+            height,
+            halfHeight
+    };
+
+    const geometry = createGeometry( sizing );
+    
+    const material = new THREE.MeshStandardMaterial( {
+            color: 0x156289,
+           emissive: 0x072534,
+            side: THREE.DoubleSide,
+            flatShading: true,
+            wireframe: true
+    } );
+
+
+    const bones = createBones( sizing );
+    
+    skeleton = new THREE.Skeleton( bones );
+    
+    skinnedMesh = new THREE.SkinnedMesh( geometry, material );
+
+    const rootBone = skeleton.bones[ 0 ];
+    
+    skinnedMesh.add( rootBone );
+
+    skinnedMesh.bind( skeleton );
+
+    scene.add( skinnedMesh );
+}
+
+function createGeometry( sizing ) {
+
+    const geometry = new THREE.CylinderGeometry(
+            5, // radiusTop
+            5, // radiusBottom
+            sizing.height, // height
+            4, // radiusSegments
+            sizing.segmentCount * 1, // heightSegments
+            true // openEnded
+    );
+
+
+    const position = geometry.attributes.position;
+
+    const vertex = new THREE.Vector3();
+
+    const skinIndices = [];
+    const skinWeights = [];
+
+    for ( let i = 0; i < position.count; i ++ ) {
+            vertex.fromBufferAttribute( position, i );
+
+            const y = ( vertex.y + sizing.halfHeight );
+
+            const skinIndex = Math.floor( y / sizing.segmentHeight );
+            const skinWeight = ( y % sizing.segmentHeight ) / sizing.segmentHeight;
+
+            skinIndices.push( skinIndex, skinIndex + 1, 0, 0 );
+            skinWeights.push( 1 - skinWeight, skinWeight, 0, 0 );
+
+    }
+
+    geometry.setAttribute( 'skinIndex', new THREE.Uint16BufferAttribute( skinIndices, 4 ) );
+    geometry.setAttribute( 'skinWeight', new THREE.Float32BufferAttribute( skinWeights, 4 ) );
+
+    return geometry;
+
+    }
+
+function createBones( sizing ) {
+
+    bones = [];
+
+    let prevBone = new THREE.Bone();
+    bones.push( prevBone );
+    prevBone.position.y = - sizing.halfHeight;
+
+    for ( let i = 0; i < sizing.segmentCount; i ++ ) {
+        const bone = new THREE.Bone();
+        bone.position.y = sizing.segmentHeight;
+        bones.push( bone );
+        prevBone.add( bone );
+        prevBone = bone;
+    }
+    return bones;
+}
+
 function animate() {
 
     renderer.setAnimationLoop( render );
